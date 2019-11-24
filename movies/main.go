259aa9eed41dbda211043721f13c4bfa51b194c3
DerAlexx <alexweb199@hotmail.de>
2019-11-24 12:36:55 +0100

    Added some basic stuff

--- movies/main.go
+++ movies/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/micro/go-micro"
+)
+
 func main() {
+	service := micro.NewService(micro.Name("movies"))
+	service.Init()
+	proto.RegisterUsersHandler(service.Server(), new()) //TODO missing handler
+
+	if err := service.Run(); err != nil {
+		fmt.Println(err)
+	}
 
 }
