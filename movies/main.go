6aaceec6f6d71665759c595b37edbcf97c01a30d
DerAlexx <alexweb199@hotmail.de>
2019-11-26 11:32:40 +0100

    Added a go.mod go.sum and some Tests

--- movies/main.go
+++ movies/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 
 	"github.com/micro/go-micro"
+	"github.com/ob-vss-ws19/blatt-4-pwn2own/movies/movies"
+	proto "github.com/ob-vss-ws19/blatt-4-pwn2own/movies/proto"
 )
 
 func main() {
 	service := micro.NewService(micro.Name("movies"))
 	service.Init()
-	proto.RegisterUsersHandler(service.Server(), new()) //TODO missing handler
+	proto.RegisterMoviesHandler(service.Server(), new(movies.MovieHandlerService))
 
 	if err := service.Run(); err != nil {
 		fmt.Println(err)
