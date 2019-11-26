628579859b71536e6e721c3dbd888ecd1383d255
schroedp <noreply>
2019-11-26 20:09:00 +0100

    proto file, main file, structure of show.go

--- show/show/show.go
+++ show/show/show.go
@@ -1,1 +1,28 @@
 package show
+
+import "sync"
+
+/*
+ShowPool contains all cinemas.
+*/
+type ShowPool struct {
+	showmap map[int64]*show
+	mutex   *sync.Mutex
+}
+
+type show struct {
+	showId  int
+	movieId int
+}
+
+/*
+NewCinemaPool creates a new CinemaPool
+*/
+func NewShowPool() *ShowPool {
+	newshowmap := make(map[int64]*show)
+
+	return &ShowPool{
+		mutex:   &sync.Mutex{},
+		showmap: newshowmap,
+	}
+}
