d6522a88d49dd558c18ffa7a9783bb79236e5c2d
DerAlexx <alexweb199@hotmail.de>
2019-12-05 11:42:51 +0100

    Fixed error in reservation

--- reservation/main.go
+++ reservation/main.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	"github.com/micro/go-micro"
-	//proto "github.com/ob-vss-ws19/blatt-4-pwn2own/reservation/proto"
+	proto "github.com/ob-vss-ws19/blatt-4-pwn2own/reservation/proto"
 	res "github.com/ob-vss-ws19/blatt-4-pwn2own/reservation/reservation"
 )
 
 func main() {
 	service := micro.NewService(micro.Name("registration"))
 	service.Init()
-	proto.RegisterUsersHandler(service.Server(), new(res.ReservatServiceHandler))
+	proto.RegisterReservationHandler(service.Server(), new(res.ReservatServiceHandler))
 
 	if err := service.Run(); err != nil {
 		fmt.Println(err)
